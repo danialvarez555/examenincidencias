35402f9a80dfbc49c95b48c58884481761b03408
danialvarez555 <daniel007b83@gmail.com>
2021-11-15 14:13:41 +0100

    c4

--- Examen_Implantacion_1/borrar.php
+++ Examen_Implantacion_1/borrar.php
@@ -7,7 +7,7 @@ if(isset($_REQUEST['borrar']))
     $num_incidencia=$_REQUEST['num_incidencia']-1; //se resta 1 porque el array empieza en 0
     $contantes=count($_SESSION['incidencias']); //para lo de no se encuentra elemento
     unset($_SESSION['incidencias'][$num_incidencia]);
-    $contdespues=count($_REQUEST['incidencias']); //para lo de no se encuentra elemento
+    $contdespues=count($_SESSION['incidencias']); //para lo de no se encuentra elemento
     if($contantes==$contdespues)
     {
         echo "No se encuentra el elemento que quieres borrar";
